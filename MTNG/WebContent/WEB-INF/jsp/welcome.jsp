4c9c5f9306f2d64689e94db1fea3cdc4b11bb148
nikitachunduru <nschunduru@oakland.edu>
2018-03-14 13:40:36 -0400

    Initial check-in of example pages.

---
+++ MTNG/WebContent/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Spring MVC Tutorial by Crunchify - Hello World Spring MVC
+	Example</title>
+<style type="text/css">
+body {
+	background-image: url('https://crunchify.com/bg.png');
+}
+</style>
+</head>
+<body>
+${message}
+</body>
+</html>
---
+++ MTNG/src/ou/secs/MTNGHelloWorld.java
@@ -0,0 +1,17 @@
+package ou.secs;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MTNGHelloWorld {
+
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld() {
+
+		String message = "<br><div style='text-align:center;'>"
+				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+		return new ModelAndView("welcome", "message", message);
+	}
+}
