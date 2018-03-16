e7b0b5e288f0ea0772dd2fa3b7e80c3ec6c05a0a
nikitachunduru <nschunduru@oakland.edu>
2018-03-16 01:01:22 -0400

    ?

--- MTNG/WebContent/WEB-INF/jsp/welcome.jsp
+++ MTNG/WebContent/WEB-INF/jsp/welcome.jsp
@@ -2,11 +2,6 @@
 <html>
 <head>
 <title>MTNG Hello World</title>
-<style type="text/css">
-body {
-	background-image: url('https://crunchify.com/bg.png');
-}
-</style>
 </head>
 <body>
 ${message}
--- MTNG/src/ou/secs/MTNGHelloWorld.java
+++ MTNG/src/ou/secs/MTNGHelloWorld.java
@@ -11,7 +11,7 @@ public class MTNGHelloWorld {
 	public ModelAndView helloWorld() {
 
 		String message = "<br><div style='text-align:center;'>"
-				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+				+ "<h3>Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
 		return new ModelAndView("welcome", "message", message);
 	}
 }
