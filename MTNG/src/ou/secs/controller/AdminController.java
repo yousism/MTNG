4ba17d4dc891e81d8d6367fd8b6bd60b57aaea8f
nikitachunduru <nschunduru@oakland.edu>
2018-03-19 19:06:43 -0400

    Edited controllers

--- MTNG/src/ou/secs/controller/AdminController.java
+++ MTNG/src/ou/secs/controller/AdminController.java
@@ -3,18 +3,34 @@ package ou.secs.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AdminController {
 
 	@RequestMapping(value = "/createPoll")
 	public String createPoll(ModelMap model) {
-
 		String message = "<br><div style='text-align:center;'>"
 				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
 		return "weclome";
 	}
 
+	@RequestMapping(value = "/setTitle")
+	public void editPoll(ModelMap model) {
+
+	}
+
+	@RequestMapping(value = "/setEventLocation")
+	public void setEventLocation(ModelMap model) {
+
+	}
+
+	@RequestMapping(value = "/setValidTimes")
+	public void setValidTimes(ModelMap model) {
+
+	}
+
+	@RequestMapping(value = "/deletePoll")
+	public void deletePoll(ModelMap model) {
+
+	}
 }
--- MTNG/src/ou/secs/controller/VoterController.java
+++ MTNG/src/ou/secs/controller/VoterController.java
@@ -1,8 +1,20 @@
 package ou.secs.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VoterController {
+
+	@RequestMapping(value = "/setVote")
+	public void setVote(ModelMap model) {
+
+	}
+
+	@RequestMapping(value = "/editVote")
+	public void editVote(ModelMap model) {
+
+	}
 
 }
